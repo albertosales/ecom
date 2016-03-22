d2adcc59e5949615237936460c85bc968239a094
aluno <aluno@LAB04MICRO01.dai.ifmt>
2016-03-22 15:04:16 -0400

    classes VO atualizadas

--- Ecommerce/src/main/java/br/com/sales/compra/DAO/ProdutoPromocaoDAO.java
+++ Ecommerce/src/main/java/br/com/sales/compra/DAO/ProdutoPromocaoDAO.java
@@ -5,10 +5,34 @@
  */
 package br.com.sales.compra.DAO;
 
+import br.com.sales.dao.vo.ProdutoPromocao;
+import br.com.sales.dao.vo.ProdutoVO;
+import java.util.Date;
+import java.util.List;
+
 /**
  *
  * @author aluno
  */
-public class ProdutoPromocaoDAO {
-    
+public class ProdutoPromocaoDAO extends DAO<ProdutoPromocao>{
+
+    private static ProdutoPromocaoDAO dao;
+
+    public ProdutoPromocaoDAO(Class<ProdutoPromocao> classe) { 
+        super(classe);
+    }
+
+    public static ProdutoPromocaoDAO getInstance() {
+        if (dao == null) {
+            dao = new ProdutoPromocaoDAO(ProdutoPromocao.class);
+        }
+        return dao;
+    }
+
+    public List<ProdutoVO> get(Date inicio, Date fim) {
+        String hql = "FROM ProdutoVO WHERE promocao = true";
+        return getInstance().getSession().createQuery(hql).list();
+    }
+
 }
+
